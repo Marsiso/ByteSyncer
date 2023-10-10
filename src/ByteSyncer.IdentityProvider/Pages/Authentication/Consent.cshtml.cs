be89e21d3cdba568a0477e9140eea792d13c854b
Marek Olšák <olsak.marek@outlook.cz>
2023-10-10 21:00:04 +0200

    #5 - Added consent page.

---
+++ src/ByteSyncer.IdentityProvider/Pages/Authentication/Consent.cshtml.cs
@@ -0,0 +1,40 @@
+using ByteSyncer.Domain.Constants;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpenIddict.Abstractions;
+
+namespace ByteSyncer.IdentityProvider.Pages.Authentication
+{
+    [Authorize]
+    public class ConsentModel : PageModel
+    {
+        [BindProperty]
+        public string? ReturnUrl { get; set; }
+
+        public IActionResult OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string? grant)
+        {
+            User.SetClaim(AuthorizationDefaults.ConsentNaming, grant);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, User);
+
+            if (string.IsNullOrWhiteSpace(ReturnUrl))
+            {
+                return Redirect("../Index");
+            }
+            else
+            {
+                return Redirect(ReturnUrl);
+            }
+        }
+    }
+}
