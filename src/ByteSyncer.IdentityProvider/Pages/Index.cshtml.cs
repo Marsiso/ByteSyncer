d60471655b0fbe3d46bcbc1654df0e9d42221898
Marek Olšák <olsak.marek@outlook.cz>
2023-10-13 00:43:06 +0200

    #5 - Replaced Material Design Bootstrap 5 with the Foundation for Sites.

--- src/ByteSyncer.IdentityProvider/Pages/Index.cshtml.cs
+++ src/ByteSyncer.IdentityProvider/Pages/Index.cshtml.cs
@@ -1,20 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using AutoMapper;
+using ByteSyncer.Core.Application.Commands;
+using ByteSyncer.Domain.Application.DataTransferObjects;
+using ByteSyncer.Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ByteSyncer.IdentityProvider.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IMapper mapper, IMediator mediator)
         {
-            _logger = logger;
+            _mapper = mapper;
+            _mediator = mediator;
         }
 
-        public void OnGet()
-        {
+        [BindProperty]
+        public string? ReturnUrl { get; set; }
 
+        [BindProperty]
+        public LoginInput? Form { get; set; }
+
+        public EntityValidationException? ValidationException { get; set; }
+
+        public IActionResult OnGet(string? returnUrl)
+        {
+            ReturnUrl = returnUrl;
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostLoginAsync()
+        {
+            LoginCommand command = _mapper.Map<LoginCommand>(Form);
+            LoginCommandResult commandResult = await _mediator.Send(command);
+
+            if (commandResult.Result == LoginCommandResultType.Succeded)
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new(ClaimTypes.Email, commandResult.User.Email),
+                    new(ClaimTypes.GivenName, commandResult.User.GivenName),
+                    new(ClaimTypes.Surname, commandResult.User.FamilyName)
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+                if (string.IsNullOrWhiteSpace(ReturnUrl))
+                {
+                    return Redirect("/Index");
+                }
+                else
+                {
+                    return Redirect(ReturnUrl);
+                }
+            };
+
+            ValidationException = commandResult?.Exception as EntityValidationException;
+
+            return Page();
+        }
+
+        public IActionResult OnPostSetCulture(string? culture, string? redirectUri)
+        {
+            return Page();
+        }
+
+        public bool HasValidationErrors([NotNullWhen(true)] string? propertyName)
+        {
+            bool hasValidationErrors = ValidationException is not null && ValidationException.Errors.ContainsKey(propertyName);
+
+            return hasValidationErrors;
+        }
+
+        public string GetInputValidationErrorClasses(string? propertyName)
+        {
+            string classes = string.Empty;
+
+            if (HasValidationErrors(propertyName))
+            {
+                classes = "is-invalid";
+            }
+
+            return classes;
+        }
+
+        public string GetFirstValidationError(string? propertyName)
+        {
+            string message = string.Empty;
+
+            if (HasValidationErrors(propertyName))
+            {
+                message = ValidationException.Errors[propertyName].First();
+            }
+
+            return message;
         }
     }
 }
---
+++ src/ByteSyncer.IdentityProvider/Pages/Register.cshtml.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
+using ByteSyncer.Core.Application.Commands;
+using ByteSyncer.Domain.Application.DataTransferObjects;
+using ByteSyncer.Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ByteSyncer.IdentityProvider.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public RegisterModel(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public string? ReturnUrl { get; set; }
+
+        [BindProperty]
+        public RegisterInput? Form { get; set; }
+
+        public EntityValidationException? ValidationException { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostRegisterAsync()
+        {
+            RegisterCommand command = _mapper.Map<RegisterCommand>(Form);
+            RegisterCommandResult commandResult = await _mediator.Send(command);
+
+            if (commandResult.Result == RegisterCommandResultType.Succeded)
+            {
+                return Redirect("/Index");
+            }
+
+            ValidationException = commandResult?.Exception as EntityValidationException;
+
+            return Page();
+        }
+
+        public bool HasValidationErrors([NotNullWhen(true)] string? propertyName)
+        {
+            bool hasValidationErrors = ValidationException is not null && ValidationException.Errors.ContainsKey(propertyName);
+
+            return hasValidationErrors;
+        }
+
+        public string GeInputValidationErrorClasses(string? propertyName)
+        {
+            string classes = string.Empty;
+
+            if (HasValidationErrors(propertyName))
+            {
+                classes = "is-invalid";
+            }
+
+            return classes;
+        }
+
+        public string GetFirstValidationError(string? propertyName)
+        {
+            string message = string.Empty;
+
+            if (HasValidationErrors(propertyName))
+            {
+                message = ValidationException.Errors[propertyName].First();
+            }
+
+            return message;
+        }
+    }
+}
